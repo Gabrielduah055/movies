89201fff393aa1abf1130ac68df30f58009649f7
Gabriel Duah <gabriel.duah@amalitech.com>
2024-05-23 17:24:02 +0200

    login and signup page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:LoginComponent,
+        title:'login'
+    },
+
+    {
+        path:'signup',
+        component:RegisterComponent,
+        title:'register'
+    }
+];
