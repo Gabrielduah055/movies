4e9103e6de856a07da2a57aecd8d1b08b637ab01
Gabriel Duah <gabriel.duah@amalitech.com>
2024-05-24 10:30:11 +0200

    created custom email validation

--- src/app/Pages/register/register.component.ts
+++ src/app/Pages/register/register.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import {
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+  FormControl,
+  NonNullableFormBuilder,
+} from '@angular/forms';
+import { PasswordComponent } from '../../Components/password/password.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink, PasswordComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  fb = inject(FormBuilder);
+  register = this.fb.group({
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    repeatPassword: ['', [Validators.required]],
+  });
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(): void {
+    console.log(this.register.getRawValue());
+  }
+
+  get emailRequired() {
+    return (
+      this.register.get('email')?.touched &&
+      this.register.get('email')?.hasError('required')
+    );
+  }
+
 
 }
