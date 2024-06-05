ec7aa9a01f01ac64b30a93b62dd68e8afbd4fb8a
Gabriel Duah <gabriel.duah@amalitech.com>
2024-06-05 10:30:37 +0200

    router active done done

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  router = inject(Router)
+
+  navItems = [
+    { path: '/home', icon: 'icon-nav-home.svg', activeIcon: 'shape.svg' },
+    { path: '/movies', icon: 'icon-nav-movies.svg', activeIcon: 'movies.svg' },
+    { path: '/series', icon: 'icon-nav-tv-series.svg', activeIcon: 'tv.svg' },
+    { path: '/bookmark', icon: 'icon-nav-bookmark.svg', activeIcon: 'bookmark.svg' }
+  ]
+  
 }
