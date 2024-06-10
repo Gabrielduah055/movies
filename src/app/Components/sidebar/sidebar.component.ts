4634f90f0ab3ba36501db6b4e52c7f2d5fa22fee
Gabriel Duah <gabriel.duah@amalitech.com>
2024-06-10 10:18:21 +0200

    hover effect on the icons done

--- src/app/Components/sidebar/sidebar.component.ts
+++ src/app/Components/sidebar/sidebar.component.ts
@@ -15,10 +15,11 @@ export class SidebarComponent {
   router = inject(Router)
 
   navItems = [
-    { path: '/home', icon: 'icon-nav-home.svg', activeIcon: 'Shape.svg' },
-    { path: '/movies', icon: 'icon-nav-movies.svg', activeIcon: 'movies.svg' },
-    { path: '/series', icon: 'icon-nav-tv-series.svg', activeIcon: 'tv.svg' },
-    { path: '/bookmark', icon: 'icon-nav-bookmark.svg', activeIcon: 'bookmark.svg' }
+    { path: '/home', icon: 'icon-nav-home.svg', hoverIcon:'hover-home-icon.png', activeIcon: 'Shape.svg' },
+    { path: '/movies', icon: 'icon-nav-movies.svg', hoverIcon:'hover-movie-icon.png', activeIcon: 'movies.svg' },
+    { path: '/series', icon: 'icon-nav-tv-series.svg', hoverIcon:'hover-tv-icon.png', activeIcon: 'tv.svg' },
+    { path: '/bookmark', icon: 'icon-nav-bookmark.svg', hoverIcon:'hover-bookmark-icon.png', activeIcon: 'bookmark.svg' }
   ]
   
+  hoveredItem:any = null;
 }
